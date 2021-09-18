8cf6dd4469087c2fc51f886a907dbb143d288f10
Arhum Shahzad <82200170+Arhum2@users.noreply.github.com>
2021-09-18 19:23:43 -0400

    Co-authored-by: david8739 <david8739@users.noreply.github.com>

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -4,8 +4,8 @@ const LandingPage = () => {
   return (
     <div className="main">
       <h1>Refugee Response</h1>
-      <button className="button">I'm looking to help</button>
-      <button className="button">I'm seeking help</button>
+      <button className="button center" >I'm looking to help</button>
+      <button className="button center">I'm seeking help</button>
     </div>
   );
 };
