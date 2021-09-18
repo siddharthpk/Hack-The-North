9a7d83c7b8651ba1b5c438c33a29ed3da329f305
david8739 <Davidmendez1738@gmail.com>
2021-09-18 18:48:34 -0400

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,10 @@
 import './Header.css';
-//import logo from '../../assets/logo.png';
-import Button from './Button';
 
 const Header = (props) => {
     return(
         <header className="header">
             <h1>Refugee Response</h1>
-            <Button>Log In</Button>
+            <button className="btn">Log In</button>
         </header>
     )
 }
