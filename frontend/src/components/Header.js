7a4a3bfa097880cc4b8374271d9901ef6b08d32e
Arhum Shahzad <82200170+Arhum2@users.noreply.github.com>
2021-09-18 22:48:50 -0400

    logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,15 @@
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import React from 'react';
+import logo from '../logo.png';
+
 
 const Header = (props) => {
     return(
         <header className="header">
-            <h1>Refugee Response</h1>
+            <img  className="header img" src={logo} alt="logo"/>
+            <h1 className="center">Refugee  Refugee Response</h1>
             <button className="btn" component={Link} to ="/login">Login</button>
         </header>
     )
